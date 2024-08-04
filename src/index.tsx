8b1566667068acb513bddeb3489924f7949659a4
Gary Hill <garyahill.2023@gmail.com>
2024-08-03 22:52:29 -0600

    turning on react strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,6 @@ import { AppProvider } from "./AppContext";
 const el = document.getElementById("root")!;
 const root = ReactDOM.createRoot(el);
 root.render(
-	// TODO: Turn on React Strict Mode for deployment; Beautiful DnD (drag and drop)
-	// is not compatible with Strict Mode
 	<React.StrictMode>
 		<AppProvider>
 			<App />
